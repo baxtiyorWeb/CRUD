7cd14ac0aea51b5b2ecd3504b73617790ebdfd87
baxtiyorWeb <baxtiyorqurbonnazarov33@gmail.com>
2023-12-27 17:13:02 +0500

    this is CRUD app updated

--- app/hooks/useDelete.tsx
+++ app/hooks/useDelete.tsx
@@ -1,11 +1,16 @@
 import axios from "axios";
+import useGetUsers from "./useGetUsers";
+import { useEffect } from "react";
 
 
 export default function useDelete() {
-
+  const { setIsLoading } = useGetUsers()
   const userDelete = async (id: number) => {
+    setIsLoading(true)
     const response = axios.delete(`https://63a5c4fcf8f3f6d4abff62b9.mockapi.io/api/v1/CRUD/${id}`)
+    setIsLoading(false)
   }
+
   return { userDelete }
 }
 
--- app/page.tsx
+++ app/page.tsx
@@ -30,7 +30,7 @@ export default function Home() {
 
       <div>
         <div className="w-full flex justify-end mt-3 mb-3">
-          <input type="text" onChange={(e) => setSearch(e.target.value)} placeholder="search users" className="p-3 w-[300px] mr-5" />
+          <input type="text" onChange={(e) => setSearch(e.target.value)} placeholder="search users" className="p-3 w-[300px] mr-5 border border-slate-500 outline-none rounded-sm" />
         </div>
 
         {isLoading ? <h2 className="text text-center mt-5">loading ...</h2> : user.length === 0 ? <h2 className="text text-center mt-5">empty data</h2> : <table id="table" className="table-hover table ">
